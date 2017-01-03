ea75235fa0ebbb9398f1055124f539640e964d25
Daniel Normandia <danielalbertnormandia@gmail.com>
2017-01-02 22:30:48 -0500

    add Header html and render props in App

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -6,9 +6,9 @@ import Inventory from './Inventory';
 class App extends React.Component {
   render() {
     return (
-      <div className='catch-of-the-day'>
-        <div className='menu'>
-          <Header />
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
         <Inventory />
