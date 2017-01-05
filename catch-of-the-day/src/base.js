a229c2a998401eabeff3dfd05ee30ffa732678c4
Daniel Normandia <danielalbertnormandia@gmail.com>
2017-01-04 20:03:37 -0500

    add base.js, connect app to Firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCeG1mcKMX9uE5HdKvvx0DDXWHvvahlnII",
+  authDomain: "catch-of-the-day-dan-normandia.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-dan-normandia.firebaseio.com"
+});
+
+export default base;
